7b72b348848dfa9675b441ae7c2cba14c338ad2a
Niklas Choinowski <niklas.choinowski@gmail.com>
2023-05-09 12:46:23 +0200

    Finished Gui

--- ExcelToXML/Form1.cs
+++ ExcelToXML/Form1.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
 
 namespace ExcelToXML
 {
-    public partial class Form1 : Form
+    public partial class FormMainWindow : Form
     {
-        public Form1()
+        public FormMainWindow()
         {
             InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                ofd.FilterIndex = 1;
+                ofd.ShowDialog();
+                var file = ofd.OpenFile();
+                //readCSV(file);
+            }
+            dataGridView1.Rows.Add("5","Alex","Plea","02161/334455","2366","1","22.08.2000","100","Bernd","Peters","657","Hasselblad 501 CM body","2235","2","19");   
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
