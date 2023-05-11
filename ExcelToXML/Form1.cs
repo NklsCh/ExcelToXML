f706585fb50a25fb6024d60fce1b789a60837033
Niklas Choinowski <niklas.choinowski@gmail.com>
2023-05-11 12:08:24 +0200

    ReadCSV and writeCSV function
    Implemented those funtions

--- ExcelToXML/Form1.cs
+++ ExcelToXML/Form1.cs
@@ -9,29 +9,7 @@ namespace ExcelToXML
         public FormMainWindow()
         {
             InitializeComponent();
-            Debug.WriteLine("Form Intialized");
         }
-        public static string[,] readCSV(System.IO.Stream file, string path)
-        {
-            using (StreamReader parser = new StreamReader(path))
-            {
-                string[] rows = parser.ReadToEnd().Split(new string[] { "\r\n", "\n", "\r" }, StringSplitOptions.None);
-                int rowLength = rows[0].Split(',').Length;
-                string[,] table = new string[rows.Length, rowLength];
-                string header = rows[0];
-                for(int i = 0; i < rows.Length - 1; i++) // Iterating over each row
-                {
-                    string[] cells = rows[i].Split(',');
-                    for(int y = 0; y < rowLength; y++) // Iterating over each cell in row
-                    {
-                        table[i, y] = cells[y].Trim(); // Adding cells to 2D Array
-                    }
-                }
-                return table;
-            }
-
-        }
-
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -39,9 +17,8 @@ namespace ExcelToXML
             ofd.FilterIndex = 1;
             ofd.ShowDialog();
             var file = ofd.OpenFile();
-            //readCSV(file, patV;
             Functions f = new Functions();
-            //f.writeCSV(file, dataGridView1);  
+            f.writeCSV(f.readCSV(file, ofd.FileName), dataGridView1);
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
