77d2fe73c7609c40f7d2099e65c9620e3d9eadd6
Niklas Choinowski <niklas.choinowski@gmail.com>
2023-05-16 12:29:23 +0200

    WriteXML

--- ExcelToXML/Functions.cs
+++ ExcelToXML/Functions.cs
@@ -69,27 +69,38 @@ namespace ExcelToXML
 
         public void genXML(string[,] table)
         {
-            string KD_ID = "";string KD_Vorname = "";string KD_Nachname = "";int KD_Telefon = 0;int Bestell_NR = 0;int Position = 0;string Bestelldatum = "";int B_ID = 0;string B_Vorname = "";string B_Nachname = "";int B_Telefon = 0;string Artikel = "";int Nettopreis = 0;int Anzahl = 0;int Steuersatz = 0;
-
-            string template = $"<Bestellung Nummer {Bestell_NR}> \n<Kunden ID {KD_ID}> \n\t<Vorname>{KD_Vorname}</Vorname> \n\t<Nachname>{KD_Nachname}</Nachname>" +
-                              $" \n\t<Telefon>{KD_Telefon}</Telefon> \n</Kunden ID> \n<Bestellung> \n\t<Position>" +
-                              $"{Position}</Position> \n\t<Bestelldatum>{Bestelldatum}</Bestelldatum> \n</Bestellung>" +
-                              $"\n<Bearbeiter ID {B_ID}> \n\t<Vorname>{B_Vorname}</Vorname> \n\t<Nachname>{B_Nachname}</Nachname>" +
-                              $" \n\t<Telefon>{B_Telefon}</Telefon> \n</Bearbeiter ID> \n<Artikel {Artikel}> \n\t<Nettopreis>{Nettopreis}" +
-                              $"</Nettopreis> \n\t<Anzahl>{Anzahl}</Anzahl> \n\t<Steuersatz>{Steuersatz}</Steuersatz> \n</Artikel> \n</Bestellung Nummer>";
-
-            try{
-                using (StreamWriter writer = new StreamWriter(@"C:\Users\SATO02\Desktop\newfile.xml"))
+            try
+            {
+                string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                using (StreamWriter writer = new StreamWriter(Path.Combine(docPath, "file.xml")))
                 {
-                    for (int i = 1; i < table.GetLength(1) - 1; i++)
+                    writer.WriteLine("<?xml version =\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>\r\n<Root>");
+                    for (int i = 1; i < table.GetLength(0); i++)
                     {
-                        KD_ID = table[i, 0];
-                        KD_Vorname = table[i, 1]; KD_Nachname = table[i, 2]; KD_Telefon = Convert.ToInt32(table[i, 3]); Bestell_NR = Convert.ToInt32(table[i, 4]); Position = Convert.ToInt32(table[i, 5]); Bestelldatum = table[i, 6]; B_ID = Convert.ToInt32(table[i, 7]); B_Vorname = table[i, 8]; B_Nachname = table[i, 9]; B_Telefon = Convert.ToInt32(table[i, 10]); Artikel = table[i, 11]; Nettopreis = Convert.ToInt32(table[i, 12]); Anzahl = Convert.ToInt32(table[i, 13]); Steuersatz = Convert.ToInt32(table[i, 14]);
+                        string template = $"\t<BESTELLUNG>" +
+                                          $"\n\t\t<KD_ID>{table[i, 0]}</KD_ID>" +
+                                          $"\n\t\t<KD_VORNAME>{table[i, 1]}</KD_VORNAME>" +
+                                          $"\n\t\t<KD_NACHNAME>{table[i, 2]}</KD_NACHNAME>" +
+                                          $"\n\t\t<KD_TELEFON>{table[i, 3]}</KD_TELEFON>" +
+                                          $"\n\t\t<BESTELL_NR>{table[i, 4]}</BESTELL_NR>" +
+                                          $"\n\t\t<POSITION>{table[i, 5]}</POSITION>" +
+                                          $"\n\t\t<BESTELLDATUM>{table[i, 6]}</BESTELLDATUM>" +
+                                          $"\n\t\t<B_ID>{table[i, 7]}</B_ID>" +
+                                          $"\n\t\t<B_VORNAME>{table[i, 8]}</B_VORNAME>" +
+                                          $"\n\t\t<B_NACHNAME>{table[i, 9]}</B_NACHNAME>" +
+                                          $"\n\t\t<B_TELEFON>{table[i, 10]}</B_TELEFON>" +
+                                          $"\n\t\t<ARTIKEL>{table[i, 11]}</ARTIKEL>" +
+                                          $"\n\t\t<NETTOPREIS>{table[i, 12]}</NETTOPREIS>" +
+                                          $"\n\t\t<ANZAHL>{table[i, 13]}</ANZAHL>" +
+                                          $"\n\t\t<STEUERSATZ>{table[i, 14]}</STEUERSATZ>" +
+                                          $"\n\t</BESTELLUNG>";
+                                            
                         writer.WriteLine(template);
                     }
+                    writer.WriteLine("</Root>");
                 }
             }
-            catch(Exception exp)
+            catch (Exception exp)
             {
                 Debug.WriteLine(exp.Message);
             }
